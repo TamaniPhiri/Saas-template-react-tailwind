8982c1f0bc61d9f81ce15339cc7bda18c6c5f226
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-18 22:37:01 +0200

    changes

--- src/components/Global/Header.tsx
+++ src/components/Global/Header.tsx
@@ -1,14 +1,18 @@
-interface HeaderProps{
-    widthFull?:boolean
-    center?:boolean
-    content?:string
+interface HeaderProps {
+  widthFull?: boolean;
+  center?: boolean;
+  content?: string;
 }
-const Header:React.FC<HeaderProps> = ({widthFull,center,content}) => {
+const Header: React.FC<HeaderProps> = ({ widthFull, center, content }) => {
   return (
-    <h1 className={`${widthFull?"w-full":""} ${center?"text-center":"text-left"} text-[32px] md:text-[36px] lg:text-[40px]`}>
-        {content}
+    <h1
+      className={`${widthFull ? "w-full" : ""} ${
+        center ? "text-center" : "text-left"
+      } text-[32px] md:text-[36px] lg:text-[40px]`}
+    >
+      {content}
     </h1>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
