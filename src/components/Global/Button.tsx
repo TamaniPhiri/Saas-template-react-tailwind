7968486a79523062e972845186b578c9247438d3
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-18 22:44:42 +0200

    changes

--- src/components/Global/Button.tsx
+++ src/components/Global/Button.tsx
@@ -17,8 +17,8 @@ const Button: React.FC<ButtonProps> = ({
     <div
       className={`${widthFull ? "w-full" : ""} ${
         secondary ? "bg-[#b6ff9c] text-black" : ""
-      } ${primary ? "bg-[#1a1a1a]" : ""} p-3 rounded-full ${
-        plain ? "p-0 rounded-none" : ""
+      } ${primary ? "bg-[#1a1a1a]" : ""} rounded-full ${
+        plain ? "p-0 rounded-none" : "py-3 px-5"
       }`}
     >
       {content}
