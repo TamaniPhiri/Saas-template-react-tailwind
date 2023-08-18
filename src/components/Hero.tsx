b71dfcfcd2bca4b13e8bb43093ddc58f87d34512
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-18 23:48:52 +0200

    changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,20 @@
 const Hero = () => {
   return (
-    <div className="flex items-center justify-center w-full px-4 md:px-12">
-
+    <div className="flex items-center min-h-screen relative justify-center w-full px-4 md:px-12">
+      <img
+        src="https://assets.website-files.com/62bea764d94f5f7e03ba6535/62beab4349f925338e0e00c0_Hero%20left%20gradient.png"
+        alt="bgRight"
+        loading="lazy"
+        className=" absolute left-0"
+      />
+      <img
+        src="https://assets.website-files.com/62bea764d94f5f7e03ba6535/62beab431cc502479aec7741_Hero%20right%20gradient.png"
+        alt="bgRight"
+        loading="lazy"
+        className=" absolute right-0"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
