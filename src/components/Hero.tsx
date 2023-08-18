bddbb4b2969f23a0a60c5a2edbe62e4249c80ddc
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-19 00:02:26 +0200

    changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
         src="https://assets.website-files.com/62bea764d94f5f7e03ba6535/62beab4349f925338e0e00c0_Hero%20left%20gradient.png"
         alt="bgRight"
         loading="lazy"
-        className=" absolute left-0"
+        className=" absolute left-0 z-0"
       />
       <img
         src="https://assets.website-files.com/62bea764d94f5f7e03ba6535/62beab431cc502479aec7741_Hero%20right%20gradient.png"
@@ -20,7 +20,7 @@ const Hero = () => {
         <h1 className="text-[52px] leading-tight mb-4 md:text-[62px] lg:text-[72px] text-center font-bold">Build your very own SaaS <br className="lg:block"/> website with <span className="bg-clip-text text-transparent bg-gradient-to-b from-lime-400 via-white to-purple-500">Dalma</span></h1>
         <Paragraph content="Lorem ipsum dolor sit amet, consectetur adipiscing elit." center/>
       </div>
-      <div className="flex justify-center my-8 items-center gap-3">
+      <div className="flex justify-center z-10 my-8 items-center gap-3">
         <Button content="Get started" secondary/>
         <Button content="Sign up" primary/>
       </div>
