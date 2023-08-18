12b27feefbb9ea6210770ad1dfb772982285e805
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-18 17:52:40 +0200

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css'
+import Aperture from './components/Aperture'
+import Features from './components/Features'
+import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import Pricing from './components/Pricing'
 
 function App() {
 
   return (
     <>
       <Navbar/>
+      <Hero/>
+      <Features/>
+      <Aperture/>
+      <Pricing/>
+      
     </>
   )
 }
