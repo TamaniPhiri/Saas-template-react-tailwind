ec9f3be867163e4810f2f690194deb6977626380
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-18 17:53:20 +0200

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
-import './App.css'
-import Aperture from './components/Aperture'
-import Cta from './components/Cta'
-import Features from './components/Features'
-import Footer from './components/Footer'
-import Hero from './components/Hero'
-import Navbar from './components/Navbar'
-import Pricing from './components/Pricing'
-import PricingTable from './components/PricingTable'
+import "./App.css";
+import Aperture from "./components/Aperture";
+import Cta from "./components/Cta";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import PricingTable from "./components/PricingTable";
 
 function App() {
-
   return (
     <>
-      <Navbar/>
-      <Hero/>
-      <Features/>
-      <Aperture/>
-      <Pricing/>
-      <PricingTable/>
-      <Cta/>
-      <Footer/>
+      <Navbar />
+      <Hero />
+      <Features />
+      <Aperture />
+      <Pricing />
+      <PricingTable />
+      <Cta />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
