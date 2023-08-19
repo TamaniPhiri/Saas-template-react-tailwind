93cc55b28dfad7d8dbb56bda016e68f18b83ef2b
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-19 14:32:18 +0200

    changes

--- src/components/FeaturesTop.tsx
+++ src/components/FeaturesTop.tsx
@@ -3,9 +3,11 @@ import Paragraph from "./Global/Paragraph";
 
 const FeaturesTop = () => {
   return (
-    <div className="flex flex-col w-full items-center justify-center px4 md:px-12">
+    <div className="flex flex-col min-h-screen py-20 w-full items-center justify-center px4 md:px-12">
       <Header content="Cool features" center />
+      <div className="md:max-w-md max-w-xs">
       <Paragraph content="Lorem ipsum dolor sit amet, consectetur adipiscing elit." center/>
+      </div>
     </div>
   );
 };
