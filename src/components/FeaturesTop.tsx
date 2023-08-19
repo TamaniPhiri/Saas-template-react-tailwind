5ef325d335e8f506130dae9b304cb768cb2fe5ef
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-19 14:29:56 +0200

    changes

--- src/components/FeaturesTop.tsx
+++ src/components/FeaturesTop.tsx
@@ -1,11 +1,13 @@
-import Header from "./Global/Header"
+import Header from "./Global/Header";
+import Paragraph from "./Global/Paragraph";
 
 const FeaturesTop = () => {
   return (
     <div className="flex flex-col w-full items-center justify-center px4 md:px-12">
-      <Header content="Cool features" center/>
+      <Header content="Cool features" center />
+      <Paragraph content="Lorem ipsum dolor sit amet, consectetur adipiscing elit." />
     </div>
-  )
-}
+  );
+};
 
-export default FeaturesTop
+export default FeaturesTop;
