42a23f46e12d33eba7eb580a4ab2ea8acee55fc3
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-19 12:19:14 +0200

    changes

--- src/components/Global/Button.tsx
+++ src/components/Global/Button.tsx
@@ -19,7 +19,7 @@ const Button: React.FC<ButtonProps> = ({
         secondary ? "bg-[#b6ff9c] text-black" : ""
       } ${primary ? "bg-[#1a1a1a]" : ""} rounded-full ${
         plain ? "p-0 rounded-none" : "py-3 px-5"
-      } text-center hover:scale-95 active:scale-105 hover:opacity-70 transform transition-all duration-500 delay-200 cursor-pointer`}
+      } text-center hover:scale-95 active:scale-105 font-semibold hover:opacity-70 transform transition-all duration-500 delay-200 cursor-pointer`}
     >
       {content}
     </div>
