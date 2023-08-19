ce1b4e340984552f551fda7ba161946e6a28dc85
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-19 15:31:12 +0200

    changes

--- src/components/FeaturesTop.tsx
+++ src/components/FeaturesTop.tsx
@@ -14,7 +14,7 @@ const FeaturesTop = () => {
           center
         />
       </div>
-      <div className="grid lg:grid-cols-2 grid-cols-1 px-4 overflow-hidden w-full items-center justify-center gap-5">
+      <div className="grid lg:grid-cols-2 grid-cols-1 px-4 overflow-hidden w-full mt-12 items-center justify-center gap-5">
         <div className="grid grid-cols-2 bg-[#1a1a1a] overflow-hidden rounded-xl w-full items-center justify-center">
           <div className="flex pl-6 pt-5 flex-col h-full justify-between w-full">
             <div>
@@ -28,7 +28,7 @@ const FeaturesTop = () => {
           <div className="flex items-center h-full lg:w-full translate-x-20 md:translate-x-0 justify-end">
             <img
               src="https://assets.website-files.com/62bea764d94f5f7e03ba6535/62bf0bed7b23d739e54aa1da_Clipped%20graphic%201.png"
-              className="lg:object-fit object-cover object-left lg:object-center h-96"
+              className="lg:object-fit object-cover object-left lg:object-center h-80 lg:h-96"
               loading="lazy"
               alt="Feature"
             />
@@ -47,7 +47,7 @@ const FeaturesTop = () => {
           <div className="flex items-center h-full lg:w-full translate-x-20 md:translate-x-0 justify-end">
             <img
               src="https://assets.website-files.com/62bea764d94f5f7e03ba6535/62bf0bedf346dcf2b39f249d_Clipped%20graphic%202.png"
-              className="lg:object-fit object-cover object-left lg:object-center h-96"
+              className="lg:object-fit object-cover object-left lg:object-center h-80 lg:h-96"
               loading="lazy"
               alt="Feature"
             />
