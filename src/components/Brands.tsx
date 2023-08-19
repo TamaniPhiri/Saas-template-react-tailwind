35da7cfabf0dc75786ba3144a1eb07db5fd8f21f
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-19 13:40:43 +0200

    changes

--- src/components/Brands.tsx
+++ src/components/Brands.tsx
@@ -3,7 +3,7 @@ import { brands } from "../constants/Brand";
 const Brands = () => {
   return (
     <div className="h-auto flex justify-center px-4 items-center w-full overflow-hidden flex-col relative">
-        <h1>Trusted by these brands:</h1>
+      <h1 className="font-bold text-xl mb-6">Trusted by these brands:</h1>
       <div className="overflow-hidden">
         <div className="slide flex bg-black gap-16">
           {brands.map((item, index) => (
