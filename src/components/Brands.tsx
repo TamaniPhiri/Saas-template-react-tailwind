fa2d0dd6897c769c8f4cb17bf93349ec351625f9
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-19 13:39:16 +0200

    changes

--- src/components/Brands.tsx
+++ src/components/Brands.tsx
@@ -2,9 +2,10 @@ import { brands } from "../constants/Brand";
 
 const Brands = () => {
   return (
-    <div className="slider h-auto flex justify-center px-4 items-center w-full overflow-hidden relative">
-      <div className="slide-track overflow-hidden">
-        <div className="slide flex gap-16">
+    <div className="h-auto flex justify-center px-4 items-center w-full overflow-hidden flex-col relative">
+        <h1>Trusted by these brands:</h1>
+      <div className="overflow-hidden">
+        <div className="slide flex bg-black gap-16">
           {brands.map((item, index) => (
             <img key={index} src={item.image} alt={item.name} loading="lazy" />
           ))}
