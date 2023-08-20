490842fe346682a911a7efeeab0ea2546b872046
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-20 15:17:51 +0200

    changes

--- src/components/Comparison.tsx
+++ src/components/Comparison.tsx
@@ -4,7 +4,7 @@ import Paragraph from "./Global/Paragraph";
 
 const Comparison = () => {
   return (
-    <div className="flex flex-col w-full px-4 md:px-12 items-center justify-center">
+    <div className="flex flex-col w-full min-h-screen px-4 gap-3 md:px-12 items-center justify-center">
       <img
         src="https://assets.website-files.com/62bea764d94f5f7e03ba6535/62c31033212f613fae2e7092_Subheading%20line.svg"
         alt="GreenLine"
@@ -14,19 +14,20 @@ const Comparison = () => {
       <Paragraph
         content="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
         center
+        minWidth
       />
-      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 items-center justify-center w-full">
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 mt-10 grid-cols-1 items-center justify-center w-full">
         {comparisons.map((item, index) => (
           <div
             key={index}
             className="flex flex-col items-center justify-center w-full"
           >
-            <div className="bg-[#b6ff9c] rounded-full">
+            <div className="bg-[#b6ff9c] rounded-full p-3">
               <img
                 src={item.image}
                 alt={item.title}
                 loading="lazy"
-                className="object-cover object-center w-full h-full"
+                className="object-cover object-center h-10"
               />
             </div>
             <h1>{item.title}</h1>
--- src/components/Global/Paragraph.tsx
+++ src/components/Global/Paragraph.tsx
@@ -2,13 +2,14 @@ interface ParagraphProps {
   content?: string;
   center?: boolean;
   small?: boolean;
+  minWidth?:boolean;
 }
-const Paragraph: React.FC<ParagraphProps> = ({ content, center, small }) => {
+const Paragraph: React.FC<ParagraphProps> = ({ content, center, small ,minWidth}) => {
   return (
     <div
       className={`${center ? "text-center" : "text-left"} ${
         small ? "text-sm md:text-[16px]" : "text-[16px] md:text-[18px] lg:text-[20px]"
-      }`}
+      } ${minWidth?"max-w-sm":""}`}
     >
       {content}
     </div>
