452e9b7535a662196a13f60f2ab67a65aaa1b306
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-20 15:01:45 +0200

    changes

--- src/components/Comparison.tsx
+++ src/components/Comparison.tsx
@@ -1,3 +1,6 @@
+import Header from "./Global/Header";
+import Paragraph from "./Global/Paragraph";
+
 const Comparison = () => {
   return (
     <div className="flex flex-col w-full px-4 md:px-12 items-center justify-center">
@@ -6,6 +9,8 @@ const Comparison = () => {
         alt="GreenLine"
         loading="lazy"
       />
+      <Header content="What sets us apart" center/>
+      <Paragraph content="Lorem ipsum dolor sit amet, consectetur adipiscing elit." center/>
     </div>
   );
 };
