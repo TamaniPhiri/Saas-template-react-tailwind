0b5d3a8354d7c72866ce819f500511f290a1d76e
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-20 13:06:14 +0200

    changes

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -2,10 +2,10 @@ const Testimonial = () => {
   return (
     <div className="grid min-h-screen items-center md:grid-cols-2 px-4 md:px-12 grid-cols-1">
       <div className="flex w-full justify-center items-center">
-        <div className="bg-[#b6ff9c] justify-center px-8 pt-8 rounded-xl w-fit text-black flex flex-col">
+        <div className="bg-[#b6ff9c] px-8 pt-8 rounded-xl w-fit text-black flex flex-col">
           <div>
-            <h1 className="text-[24px] md:text-[28px] lg:text-[32px]">“Working with Dalma has been a breeze from the very start.”</h1>
-            <div className="flex items-center">
+            <h1 className="text-[24px] md:text-[28px] lg:text-[32px] font-bold">“Working with Dalma has been a breeze from the very start.”</h1>
+            <div className="flex items-center my-5 gap-2">
               <div>
                 <img
                   src="https://assets.website-files.com/62bea764d94f5f7e03ba6535/62c30ecd9726db58623b59a3_Small%20portrait.png"
@@ -16,7 +16,7 @@ const Testimonial = () => {
               </div>
               <div>
                 <h1>Joe Schmoe</h1>
-                <p>Director @Sitemark</p>
+                <p className="text-gray-600">Director @Sitemark</p>
               </div>
             </div>
           </div>
@@ -29,7 +29,7 @@ const Testimonial = () => {
           </div>
         </div>
       </div>
-      <div></div>
+      <div>asls</div>
     </div>
   );
 };
