a6b18c9bd368f8531ec1a0de1e5a46a6d8adad45
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-20 15:29:05 +0200

    changes

--- src/components/SoftwareFeature.tsx
+++ src/components/SoftwareFeature.tsx
@@ -1,6 +1,15 @@
 const SoftwareFeature = () => {
   return (
-    <div>SoftwareFeature</div>
+    <div className="flex min-h-screen items-center justify-center w-full px-4 md:px-12">
+      <div className="grid grid-cols-1 md:grid-cols-2 bg-[#1a1a1a] rounded-3xl h-full w-full">
+        <div>
+
+        </div>
+        <div>
+          <img src="https://assets.website-files.com/62bea764d94f5f7e03ba6535/62c7c631bb44107758dcff46_Block%20cta%20image.png" alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
 
