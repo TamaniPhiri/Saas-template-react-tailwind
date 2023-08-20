cb38b5cc64a94ea272c3070cb080627eb199d993
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-20 12:58:47 +0200

    changes

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -2,7 +2,7 @@ const Testimonial = () => {
   return (
     <div className="grid min-h-screen items-center md:grid-cols-2 px-4 md:px-12 grid-cols-1">
       <div className="flex w-full justify-center items-center">
-        <div className="bg-[#b6ff9c] items-center justify-center px-8 pt-8 rounded-xl w-fit text-black flex flex-col">
+        <div className="bg-[#b6ff9c] justify-center px-8 pt-8 rounded-xl w-fit text-black flex flex-col">
           <div>
             <h1 className="text-[24px] md:text-[28px] lg:text-[32px]">“Working with Dalma has been a breeze from the very start.”</h1>
             <div className="flex items-center">
@@ -20,7 +20,7 @@ const Testimonial = () => {
               </div>
             </div>
           </div>
-          <div className="flex w-full justify-start">
+          <div className="flex w-fit justify-start">
             <img
               src="https://assets.website-files.com/62bea764d94f5f7e03ba6535/62c30f17d05b1ed81196575e_Green%20block%20graphic.svg"
               alt="block"
