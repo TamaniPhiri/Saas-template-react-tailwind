0f577d5720b75f154dbd6ecf8220da3587569014
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-20 17:10:01 +0200

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Navbar from "./components/Navbar";
 import Testimonial from "./components/Testimonial";
 import Comparison from "./components/Comparison";
 import CoolFeatures from "./components/CoolFeatures";
+import Cta from "./components/Cta";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
       <Comparison />
       <SoftwareFeature />
       <CoolFeatures/>
+      <Cta/>
       <Footer />
     </>
   );
