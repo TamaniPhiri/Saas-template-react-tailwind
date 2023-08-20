49f038cbce0eab8d7acb29276277412f46073d02
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-20 13:13:54 +0200

    changes

---
+++ src/constants/Testimonials.tsx
@@ -0,0 +1,18 @@
+export const reviews = [
+  {
+    message:
+      "Very easy to use. I am completely blown away. Dalma is the real deal!",
+    rate: "5",
+    reviews: "129",
+  },
+  {
+    message: "Thanks to Dalma template, we've just launched our 5th website!",
+    rate: "5",
+    reviews: "129",
+  },
+  {
+    message: "Working with Dalma has been a breeze from the very start",
+    rate: "5",
+    reviews: "129",
+  },
+];
