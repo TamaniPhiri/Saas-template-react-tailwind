1f2287f439ff8c2fd8e5416db22c1fb4b5bf7013
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-20 16:45:20 +0200

    changes

--- src/components/CoolFeatures.tsx
+++ src/components/CoolFeatures.tsx
@@ -1,9 +1,13 @@
+import Header from "./Global/Header";
+import Paragraph from "./Global/Paragraph";
+
 const CoolFeatures = () => {
   return (
     <div className="flex flex-col justify-center px-4 md:px-12 w-full">
-        helo
+      <Header content="Cool features" />
+      <Paragraph content="Lorem ipsum dolor sit amet, consectetur adipiscing elit." />
     </div>
-  )
-}
+  );
+};
 
-export default CoolFeatures
+export default CoolFeatures;
