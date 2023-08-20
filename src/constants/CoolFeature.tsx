5ba78791e105869769b4b198078e9228c7197b43
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-20 16:53:00 +0200

    changes

--- src/constants/CoolFeature.tsx
+++ src/constants/CoolFeature.tsx
@@ -5,28 +5,28 @@ export const coolFeature=[
         description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
-        image:"",
-        title:"Gorgeous design",
+        image:"https://assets.website-files.com/62bea764d94f5f7e03ba6535/62cfb846cd5cb470df5b989a_icons8-Plumpy-OI6BowItHaZ4-96-000000%201.png",
+        title:"Adaptability",
         description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
-        image:"",
-        title:"Gorgeous design",
+        image:"https://assets.website-files.com/62bea764d94f5f7e03ba6535/62cfb846c58a3acf35fd45e7_icons8-Plumpy-VreP1KFWExGM-96-000000%201.png",
+        title:"Responsive",
         description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
-        image:"",
-        title:"Gorgeous design",
+        image:"https://assets.website-files.com/62bea764d94f5f7e03ba6535/62cfb8466b0d2a386cbe51c5_icons8-Plumpy-AdZ21McLt6YJ-96-000000%201.png",
+        title:"Speed optimized",
         description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
-        image:"",
-        title:"Gorgeous design",
+        image:"https://assets.website-files.com/62bea764d94f5f7e03ba6535/62cfb846cc2dddfd5c2cb727_icons8-Plumpy-T4zuCdQj9cg7-96-000000%201.png",
+        title:"Multi layout",
         description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
-        image:"",
-        title:"Gorgeous design",
+        image:"https://assets.website-files.com/62bea764d94f5f7e03ba6535/62cfb8461eea8f7e1e9ef713_icons8-Plumpy-tgKFpvPBTLey-96-000000%201.png",
+        title:"Fully customizable",
         description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
 ]
