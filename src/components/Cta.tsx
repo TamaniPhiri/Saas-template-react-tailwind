e4e067e09ab8437e9c6d9ae0b84606a16e626d5a
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-20 17:11:25 +0200

    changes

--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -1,7 +1,22 @@
+import Header from "./Global/Header";
+import Paragraph from "./Global/Paragraph";
+
 const Cta = () => {
   return (
-    <div>Cta</div>
-  )
-}
+    <div className="flex flex-col w-full min-h-screen px-4 gap-3 md:px-12 items-center justify-center">
+      <img
+        src="https://assets.website-files.com/62bea764d94f5f7e03ba6535/62c31033212f613fae2e7092_Subheading%20line.svg"
+        alt="GreenLine"
+        loading="lazy"
+      />
+      <Header content="What sets us apart" center />
+      <Paragraph
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        center
+        minWidth
+      />
+    </div>
+  );
+};
 
-export default Cta
+export default Cta;
