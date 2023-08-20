8baa7a4263f397b4ea29dc4848baa1c2e6fbbb58
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-20 15:53:07 +0200

    changes

--- src/components/SoftwareFeature.tsx
+++ src/components/SoftwareFeature.tsx
@@ -16,6 +16,32 @@ const SoftwareFeature = () => {
             <Button content="Get started" secondary widthFull={false}/>
             <Button content="Other templates" primary widthFull={false}/>
           </div>
+          <div className="flex gap-3 items-center mt-12">
+            <img
+              src="https://assets.website-files.com/62bea764d94f5f7e03ba6535/62c19cfee7fe5c224e5d1631_luminous-Color%20icon.svg"
+              alt="Luminous"
+            />
+            <img
+              src="https://assets.website-files.com/62bea764d94f5f7e03ba6535/62c19cfd8758bb7f808b95b1_Network-Color%20icon.svg"
+              alt="Network"
+            />
+            <img
+              src="https://assets.website-files.com/62bea764d94f5f7e03ba6535/62c19cfe54de256a6e45022b_Sitemark-Color%20icon.svg"
+              alt="SiteMark"
+            />
+          </div>
+          <a
+            href="#"
+            className="flex items-center group justify-between w-full py-5 border-b border-gray-800"
+          >
+            <span>Integrate your favorite software.</span>
+            <img
+              src="https://assets.website-files.com/62bea764d94f5f7e03ba6535/62c19d96b115e07d5798a0c7_Arrow%20right.svg"
+              alt="rarr"
+              loading="lazy"
+              className="group-hover:translate-x-2 transform transition-all duration-300 ease-in-out"
+            />
+          </a>
         </div>
         <div className="">
           <img
