0134e687b0c5f78955e91efdc011ba1f36369ef0
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-20 15:08:47 +0200

    changes

---
+++ src/constants/Comparisons.tsx
@@ -0,0 +1,22 @@
+export const comparisons=[
+    {
+        image:"https://assets.website-files.com/62bea764d94f5f7e03ba6535/62cfb84622f09cd426ac6e40_icons8-Plumpy-77hDlnibqiKC-96-000000%201.png",
+        title:"Gorgeous design",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        image:"https://assets.website-files.com/62bea764d94f5f7e03ba6535/62cfb8461eea8f7e1e9ef713_icons8-Plumpy-tgKFpvPBTLey-96-000000%201.png",
+        title:"Fully customizable",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        image:"https://assets.website-files.com/62bea764d94f5f7e03ba6535/62cfb846cd5cb470df5b989a_icons8-Plumpy-OI6BowItHaZ4-96-000000%201.png",
+        title:"Adaptability",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        image:"https://assets.website-files.com/62bea764d94f5f7e03ba6535/62cfb8466b0d2a386cbe51c5_icons8-Plumpy-AdZ21McLt6YJ-96-000000%201.png",
+        title:"Speed Optimized",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    }
+]
