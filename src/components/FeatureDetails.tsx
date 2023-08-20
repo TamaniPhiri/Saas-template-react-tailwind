548e5c00003d52df77d56d800f89e4e13caba4f8
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-20 11:51:08 +0200

    changes

--- src/components/FeatureDetails.tsx
+++ src/components/FeatureDetails.tsx
@@ -3,13 +3,13 @@ import Paragraph from "./Global/Paragraph";
 
 const FeatureDetails = () => {
   return (
-    <div className="flex flex-col py-24 min-h-screen w-full items-center justify-center px-4 md:px-12">
-      <div className="w-full grid md:grid-cols-2 grid-cols-1 lg:px-10">
+    <div className="flex flex-col py-24 min-h-screen md:h-auto w-full items-center justify-center px-4 md:px-12">
+      <div className="w-full lg:min-h-screen bg-red-200 justify-center items-center grid md:gap-8 md:grid-cols-2 grid-cols-1 lg:px-10">
         <div className="flex flex-col max-w-sm gap-3">
-          <span>Software feature</span>
+          <span className="text-[#b6ff9c] font-bold text-lg">Software feature</span>
           <Header content="Build quickly and easily with Dalma" />
           <Paragraph content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at eleifend senectus." />
-          <div className="flex gap-3 items-center my-4">
+          <div className="flex gap-3 items-center mt-4">
             <img
               src="https://assets.website-files.com/62bea764d94f5f7e03ba6535/62c19cfee7fe5c224e5d1631_luminous-Color%20icon.svg"
               alt="Luminous"
@@ -23,8 +23,16 @@ const FeatureDetails = () => {
               alt="SiteMark"
             />
           </div>
+          <a href="#" className="flex items-center justify-between w-full py-4 border-b border-gray-800">
+            <span>Integrate your favorite software.</span>
+            <img
+              src="https://assets.website-files.com/62bea764d94f5f7e03ba6535/62c19d96b115e07d5798a0c7_Arrow%20right.svg"
+              alt="rarr"
+              loading="lazy"
+            />
+          </a>
         </div>
-        <div className="w-full flex justify-center">
+        <div className="w-full flex justify-center md:mt-0 mt-10">
           <img
             src="https://assets.website-files.com/62bea764d94f5f7e03ba6535/62c30bd4926d58602b0b520b_Purchase%20chart%201.png"
             alt="features"
@@ -32,23 +40,29 @@ const FeatureDetails = () => {
             className=" object-cover rounded-xl bg-[#1a1a1a] p-3 md:p-6 object-center md:h-[22rem]"
           />
         </div>
-        <div>
-          <span>Integrate your favorite software.</span>
-          <img src="https://assets.website-files.com/62bea764d94f5f7e03ba6535/62c19d96b115e07d5798a0c7_Arrow%20right.svg" alt="rarr" loading="lazy"/>
-        </div>
       </div>
-      <div className="w-full grid md:grid-cols-2 grid-cols-1">
-        <div>
+      <div className="w-full lg:min-h-screen bg-red-500 justify-center items-center grid md:grid-cols-2 grid-cols-1">
+        <div className="w-full flex justify-center md:mt-0 mt-10">
           <img
             src="https://assets.website-files.com/62bea764d94f5f7e03ba6535/62c30bd4926d58c7350b520a_Purchase%20chart%202.png"
             alt="features"
             loading="lazy"
+            className=" object-cover rounded-xl bg-[#1a1a1a] p-3 md:p-6 object-center md:h-[22rem]"
           />
         </div>
-        <div className="flex flex-col gap-3">
-          <span>Software feature</span>
+        <div className="flex flex-col max-w-sm gap-3">
+          <span className="text-[#b6ff9c] font-bold text-lg">Software feature</span>
           <Header content="Create stunning websites with Dalma" />
           <Paragraph content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at eleifend senectus." />
+          <div className="flex gap-3 w-full">
+            <div className="bg-[#b6ff9c] rounded-full p-3">
+              <img src="https://assets.website-files.com/62bea764d94f5f7e03ba6535/62cfb84622f09cd426ac6e40_icons8-Plumpy-77hDlnibqiKC-96-000000%201.png" alt="" />
+            </div>
+            <div className="flex flex-col">
+              <h1>Gorgeous design</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
