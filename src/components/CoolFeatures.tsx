6961057966e258b79a89a522909fa374075ca6ca
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-20 17:08:31 +0200

    changes

--- src/components/CoolFeatures.tsx
+++ src/components/CoolFeatures.tsx
@@ -4,16 +4,29 @@ import Paragraph from "./Global/Paragraph";
 
 const CoolFeatures = () => {
   return (
-    <div className="flex flex-col justify-center px-4 md:px-12 w-full">
+    <div className="flex relative flex-col gap-2 justify-center px-4 md:px-12 w-full">
       <Header content="Cool features" />
-      <Paragraph content="Lorem ipsum dolor sit amet, consectetur adipiscing elit." />
-      <div className="grid gap-5 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 items-center justify-center w-full">
+      <Paragraph
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        minWidth
+      />
+      <div className="grid gap-5 z-40 lg:grid-cols-3 mt-10 md:grid-cols-2 grid-cols-1 items-center justify-center w-full">
         {coolFeature.map((item, index) => (
           <div
             key={index}
             className="flex flex-col items-start gap-3 bg-white/10 bg-opacity-20 backdrop-blur-sm p-5 rounded-xl w-full"
           >
-            <div className="bg-[#b6ff9c] rounded-full p-3">
+            <div
+              className={`bg-[#b6ff9c] ${
+                index === 0
+                  ? "bg-white"
+                  : index === 2
+                  ? "bg-white"
+                  : index === 4
+                  ? "bg-white"
+                  : ""
+              } rounded-full p-3`}
+            >
               <img
                 src={item.image}
                 alt={item.title}
@@ -29,6 +42,12 @@ const CoolFeatures = () => {
           </div>
         ))}
       </div>
+      <img
+        src="https://assets.website-files.com/62bea764d94f5f7e03ba6535/62beab4349f925338e0e00c0_Hero%20left%20gradient.png"
+        alt="bg"
+        loading="lazy"
+        className="absolute left-0 bottom-0 -top-20 z-10"
+      />
     </div>
   );
 };
