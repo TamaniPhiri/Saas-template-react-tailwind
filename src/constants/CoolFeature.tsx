5624cae3476da4f76c0600d8240935dfba4ee89b
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-20 16:50:27 +0200

    changes

---
+++ src/constants/CoolFeature.tsx
@@ -0,0 +1,32 @@
+export const coolFeature=[
+    {
+        image:"https://assets.website-files.com/62bea764d94f5f7e03ba6535/62cfb84622f09cd426ac6e40_icons8-Plumpy-77hDlnibqiKC-96-000000%201.png",
+        title:"Gorgeous design",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        image:"",
+        title:"Gorgeous design",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        image:"",
+        title:"Gorgeous design",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        image:"",
+        title:"Gorgeous design",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        image:"",
+        title:"Gorgeous design",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        image:"",
+        title:"Gorgeous design",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+]
