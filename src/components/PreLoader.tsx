ecd37b40a43361811e7791f5c102c46da39f38cb
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-21 19:53:39 +0200

    preloader

--- src/components/PreLoader.tsx
+++ src/components/PreLoader.tsx
@@ -40,7 +40,12 @@ const PreLoader = () => {
           animate={IsInView ? "open" : ""}
           className="text-7xl md:text-9xl flex gap-2 md:gap-7 items-center overflow-hidden pl-5 m-0 font-bold tracking-wide"
         >
-          <span>Hello.</span>
+          <motion.span
+            initial={{ clipPath: "inset(100% 0% 0% 0%)" }}
+            whileInView={{ clipPath: "inset(0% 0% 0% 0%)" }}
+          >
+            Hello
+          </motion.span>
           <motion.span
             initial={{ scale: 0, rotate: 0 }}
             whileInView={{ scale: 1, rotate: 6 }}
@@ -63,6 +68,7 @@ const PreLoader = () => {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
+                className="path"
               />
             </svg>
           </motion.span>
