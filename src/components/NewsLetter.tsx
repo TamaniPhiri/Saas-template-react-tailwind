76d246657b3f9d63c256d415fb2f26b28b219459
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-22 00:01:32 +0200

    preloade

--- src/components/NewsLetter.tsx
+++ src/components/NewsLetter.tsx
@@ -1,3 +1,5 @@
+import Button from "./Global/Button";
+
 const NewsLetter = () => {
   return (
     <div className="flex flex-col px-4 py-16">
@@ -6,6 +8,10 @@ const NewsLetter = () => {
           src="https://assets.website-files.com/62bea764d94f5f7e03ba6535/62beab43601a607bdb2b4764_logo.svg"
           alt=""
         />
+      </div>
+      <div className="flex gap-4 md:flex-row flex-col">
+        <input type="text" className=" bg-transparent border-b px-2 py-5"/>
+        <Button content="Subscribe" widthFull={false} secondary/>
       </div>
     </div>
   );
