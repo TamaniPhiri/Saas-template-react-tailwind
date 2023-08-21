41fab4ab8ba576059bbc15876a28c154967124f1
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-21 19:25:22 +0200

    preloader

--- src/components/PreLoader.tsx
+++ src/components/PreLoader.tsx
@@ -49,7 +49,7 @@ const PreLoader = () => {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              strokeWidth={1.5}
+              strokeWidth={3}
               stroke="currentColor"
               className="w-24 h-24"
             >
