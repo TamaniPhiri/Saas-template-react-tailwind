e58153203fd4e6f6aafcce8ee9fc25707ac0adfa
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-21 23:58:02 +0200

    preloade

--- src/components/NewsLetter.tsx
+++ src/components/NewsLetter.tsx
@@ -1,9 +1,14 @@
 const NewsLetter = () => {
   return (
-    <div className="flex flex-col px-4 md:px-12">
-      news
+    <div className="flex flex-col px-4 py-16">
+      <div>
+        <img
+          src="https://assets.website-files.com/62bea764d94f5f7e03ba6535/62beab43601a607bdb2b4764_logo.svg"
+          alt=""
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NewsLetter
+export default NewsLetter;
