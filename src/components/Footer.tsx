92b90f0583ccb7eb577ac25b53190086090a545f
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-22 00:03:56 +0200

    preloade

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import NewsLetter from "./NewsLetter";
 
 const Footer = () => {
   return (
-    <div className=" relative min-h-screen py-24 flex-col w-full px-4 md:px-12 items-center justify-center">
+    <div className=" relative min-h-screen py-24 flex-col w-full px-2 md:px-12 items-center justify-center">
       <img
         src="https://assets.website-files.com/62bea764d94f5f7e03ba6535/62c7eaa512be423d96d9fcae_Bottom%20corner%20gradient.png"
         alt="bg"
