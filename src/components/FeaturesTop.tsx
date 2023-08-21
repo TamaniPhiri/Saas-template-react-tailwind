a67bab44e998249e3af3f8fb1273c70ed5132259
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-21 22:22:42 +0200

    preloader

--- src/components/FeaturesTop.tsx
+++ src/components/FeaturesTop.tsx
@@ -1,8 +1,14 @@
+import { motion } from "framer-motion";
 import Button from "./Global/Button";
 import Header from "./Global/Header";
 import Paragraph from "./Global/Paragraph";
+import { useEffect, useState } from "react";
 
 const FeaturesTop = () => {
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
   return (
     <div className="flex flex-col min-h-screen py-24 w-full overflow-hidden items-center justify-center px4 md:px-12">
       <h1 className="text-[40px] md:text-[48px] lg:text-[56px] font-bold">
@@ -15,14 +21,22 @@ const FeaturesTop = () => {
         />
       </div>
       <div className="grid lg:grid-cols-2 grid-cols-1 px-4 overflow-hidden w-full mt-12 items-center justify-center gap-5">
-        <div className="grid grid-cols-2 bg-[#1a1a1a] overflow-hidden rounded-xl w-full items-center justify-center">
+        <motion.div
+          initial={loaded ? {} : { opacity: 0, scale: 0.8 }}
+          whileInView={loaded ? { opacity: 1, scale: 1 } : {}}
+          transition={{ delay: 0.3, duration: 1, type: "tween" }}
+          className="grid grid-cols-2 bg-[#1a1a1a] overflow-hidden rounded-xl w-full items-center justify-center"
+        >
           <div className="flex pl-6 pt-5 flex-col h-full justify-between w-full">
             <div>
               <Header content="Credit sending" center={false} />
-              <Paragraph content="Lorem ipsum dolor sit amet, consectetur adipiscing elit." small/>
+              <Paragraph
+                content="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                small
+              />
             </div>
             <div className="mb-12">
-              <Button content="Get started" secondary/>
+              <Button content="Get started" secondary />
             </div>
           </div>
           <div className="flex items-center h-full lg:w-full translate-x-20 md:translate-x-0 justify-end">
@@ -33,15 +47,18 @@ const FeaturesTop = () => {
               alt="Feature"
             />
           </div>
-        </div>
+        </motion.div>
         <div className="grid grid-cols-2 bg-[#1a1a1a] overflow-hidden rounded-xl w-full items-center justify-center">
           <div className="flex pl-6 pt-5 flex-col h-full justify-between w-full">
             <div>
               <Header content="Scheduling" center={false} />
-              <Paragraph content="Lorem ipsum dolor sit amet, consectetur adipiscing elit." small/>
+              <Paragraph
+                content="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                small
+              />
             </div>
             <div className="mb-12">
-              <Button content="Get started" secondary/>
+              <Button content="Get started" secondary />
             </div>
           </div>
           <div className="flex items-center h-full lg:w-full translate-x-20 md:translate-x-0 justify-end">
