fa26cece2046adbc5665c24dbb76df0b2ffe7018
Tamani Phiri <tamanigabriel0@gmail.com>
2023-08-21 18:13:10 +0200

    preloader

--- src/components/PreLoader.tsx
+++ src/components/PreLoader.tsx
@@ -8,10 +8,10 @@ const PreLoader = () => {
       transition={{ delay: 1, duration: 2 }}
       className="absolute inset-0 z-[60]"
     >
-      <div className="bg-[#b6ff9c] text-black min-h-screen items-center flex py-56">
-        <span className="text-6xl md:text-9xl font-bold tracking-wide">
+      <div className="bg-[#b6ff9c] text-black h-fit min-h-screen lineMask overflow-hidden items-center flex py-56">
+        <motion.span initial={{translateY:50}} className="text-6xl md:text-9xl overflow-hidden -mt-4 pl-5 m-0 font-bold tracking-wide">
           Hello.
-        </span>
+        </motion.span>
       </div>
     </motion.div>
   );
